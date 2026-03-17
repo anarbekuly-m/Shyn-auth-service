72eb6eccb55638914158c9a7cf53f091fcfaea7a
meirzhan <meirzhan.anarbekuly@gmail.com>
2026-03-17 15:32:46 +0500

    permit

--- src/main/java/pro/hiking/auth/config/SecurityConfig.java
+++ src/main/java/pro/hiking/auth/config/SecurityConfig.java
@@ -27,15 +27,5 @@ public class SecurityConfig {
         return http.build();
     }
 
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins("*")
-                        .allowedMethods("*");
-            }
-        };
-    }
+
 }
